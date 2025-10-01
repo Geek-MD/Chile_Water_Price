a77a7b8422dc83ba17b6531c5ca15e14e3f04b51
Edison Montes M. <edison.montes@gmail.com>
2025-10-01 19:13:29 -0300

    Delete custom_components/siss_tariffs directory

--- custom_components/siss_tariffs/const.py
+++
@@ -1,7 +0,0 @@
-DOMAIN = "siss_tariffs"
-
-SISS_URL = "https://www.siss.gob.cl/586/w3-propertyvalue-6385.html"
-HTML_FILE = "custom_components/siss_tariffs/siss_source.html"
-TARIFFS_FILE = "custom_components/siss_tariffs/siss_tariffs.json"
-
-UPDATE_INTERVAL_DAYS = 7
